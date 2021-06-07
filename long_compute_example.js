59847c3ac8dbf192b68cb3ec678a3149c489507e
hanshenSun <hanshensunw@gmail.com>
2021-06-07 16:42:47 -0400

    Update long_compute_example.js

--- long_compute_example.js
+++ long_compute_example.js
@@ -2,26 +2,30 @@
 // Swarm App: https://swarm.thorntontomasetti.com/app/5ef067beeba6c0821cddd2e6/info
 
 var Swarm = require('@ttcorestudio/swarm');
-var swarmApp = new Swarm();
+var swarmApp = new Swarm.SwarmApp();
 swarmApp.setDocument(8, 0.001); // Set Document unit and tolerance
 
-swarmApp.userId = "5e30091214c7ae0004f2fac2";
+// swarmApp.userId = "5da0c032997b8c000432e6bf";
+// swarmApp.ssoId = "595eb281604ca5f75fd3bd17";
 swarmApp.appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTE5MzY3MTE0MzgsImV4cCI6MTYxMTk0MTg5NTQzOCwicHJvamVjdElkIjoiNWVmMDY3YmVlYmE2YzA4MjFjZGRkMmU2In0.XpFDmH9_KVghshXOXmEsPUnYksDOlPqPoiGy2u4lKPA";
 
-swarmApp.addInput({
-  type: "Slider",
-  name: "Snooze_ms",
-  values: [{ // tree structure
-    Value: 10000
-  }]
-});
+
+// Declare inputs first
+let inputA = new Swarm.Input("Snooze_ms", "Slider");
+let inputB = new Swarm.Input("B", "Number");
+
+
+inputA.addData(1000);
+
+
 
 // Sending to Swarm for compute
 swarmApp.runLongCompute().then(output => {
-  let val = output.outputList;
+  console.log("output", output.outputs[0].outputValue);
+  // let val = output.outputList;
 
-  val.forEach(v => {
-    console.log("Output Name: ", v.name);
-    console.log("Output Value: ", v.outputValue);
-  })
+  // val.forEach(v => {
+  //   console.log("Output Name: ", v.name);
+  //   console.log("Output Value: ", v.outputValue);
+  // })
 });
